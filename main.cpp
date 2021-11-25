8c88a2f6242dbeac9d8f69390a635a65d547fb64
Nux-xader <sumgmng@gmail.com>
2021-11-25 16:42:32 +0700

    create admin menu, create func for check already account

--- main.cpp
+++ main.cpp
@@ -1,9 +1,23 @@
+// include standard library
 #include <iostream>
 #include <fstream>
 #include <string>
 
 using namespace std;
+// declarate banner var
+string banner = "\tRESTO APP V1.0.0\n";
 
+
+// Function for clear screen
+void clr_screen() {
+	// if oprating system is windows 32bit or 64bit use cls
+	#ifdef _WIN32
+		system("cls");
+	// if oprating system not windows use clear
+	#else
+	    system("clear");
+	#endif
+}
 
 // return admin if login as admin, and return karyawan if login as karyawan
 string login(string username, string password) {
@@ -53,21 +67,116 @@ string login(string username, string password) {
 }
 
 
-// Function for clear screen
-void clr_screen() {
-	// if oprating system is windows 32bit or 64bit use cls
-	#ifdef _WIN32
-		system("cls");
-	// if oprating system not windows use clear
-	#else
-	    system("clear");
-	#endif
+// Check if account already regristed
+bool check_already_account(string username, string type_account) {
+	// decarate var
+	ifstream admin, karyawan;
+	string data;
+
+	// Check admin account
+	if (type_account == "admin") {
+		int i = 0;
+		admin.open("db/admin.txt");
+		while(!admin.eof()) {
+			i++;
+			admin >> data;
+			if ((i%2) != 0) {
+				if (username == data) {
+					return true;
+				}
+			}
+		}
+		return false;
+
+	} else if (type_account == "karyawan") {
+		// Check karyawan account
+		int i = 0;
+		karyawan.open("db/karyawan.txt");
+		while(!karyawan.eof()) {
+			i++;
+			karyawan >> data;
+			if ((i%2) != 0) {
+				if (username == data) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	return false;
+}
+
+
+
+void add_admin_account() {
+	string username, password, add_again;
+	while (true) {
+		clr_screen();
+		cout << banner << endl;
+		cout << "Tambah akun admin" << endl;
+
+		// always check username if already regristed
+		while (true) {
+			cout << "Username : ";
+			cin >> username;
+			// Checking username
+			if (check_already_account(username, "admin")) {
+				cout << "Akun "<< username <<"telah terdaftar sebelumnya!" << endl;
+				cout << "Gunakan username lain untuk menambahkan akun baru" << endl;
+			} else { // braek if username not already
+				break;
+			}
+		}
+
+		// input password
+		cout << "Password : ";
+		cin >> password;
+		cout << "Akun berhasil di tambahkan" << endl;
+		cout << "Apakah ingin menambahkan akun lagi? (y) : ";
+		cin >> add_again;
+		if ((add_again == "y") | (add_again == "Y")) {
+
+		} else {
+			break;
+		}
+	}
+}
+
+
+void admin_menu() {
+	clr_screen();
+	cout << banner << endl;
+	cout << "Menu :" << endl;
+	cout << "1. Tambah akun admin" << endl;
+	cout << "2. Ubah password admin" << endl;
+	cout << "3. Tambah akun karyawan" << endl;
+	cout << "4. Ubah password karyawan" << endl;
+	cout << "5. Lihat data karyawan" << endl;
+	cout << "0. Keluar\n" << endl;
+}
+
+void admin() {
+	string choice;
+	admin_menu();
+	while (true) {
+		cout << "Pilih : ";
+		cin >> choice;
+		if (choice == "0"){
+			break;
+		} else if (choice == "1") {
+			add_admin_account();
+			admin_menu();
+		} else {
+			cout << "menu berikutnya masih tahap pengembangan" << endl;
+			break;
+		}
+	}
 }
 
 int main(){
 	// declarate variable
 	string username, password, x;
-	string banner = "\tRESTO APP V1.0.0\n";
 	while (true) {
 		clr_screen();
 		cout << banner << endl;
@@ -81,22 +190,21 @@ int main(){
 
 		// Check if login as admin
 		if (login(username, password) == "admin") {
-			cout << "Anda login sebagai admin" << endl;
-			cout << "menu selanjutnya masih tahap pengembangan" << endl;
+			admin();
 			break;
-		// Check if login as karyawan
-		} else if (login(username, password) == "karyawan") {
+		} else if (login(username, password) == "karyawan") { // Check if login as karyawan
 			cout << "Anda login sebagai karyawan" << endl;
 			cout << "menu selanjutnya masih tahap pengembangan" << endl;
 			break;
-		// statment for wrong username or password
-		} else {
+		} else { // statment for wrong username or password
 			cout << "Wrong username or password" << endl;
 			cout << "ingin login ulang? (y/n) : ";
 			cin >> x;
-			if (x == "n" | x == "N" ) {
+			if ((x == "n") | (x == "N")) {
 				break;
-			} else if (x != "y" | x != "Y" ) {
+			} else if ((x == "y") | (x == "Y")) {
+
+			} else {
 				cout << "Anda memasukkan inputan yang salah" << endl;
 				break;
 			}
